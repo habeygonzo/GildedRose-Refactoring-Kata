21cc6d0f378b3d084ec16ca9017df2160ed39622
habeygonzo <habeygonzo@ice-wind.be>
2018-11-29 22:40:36 +0100

    - Wrote tests with expected behaviour - Initial refactor to simplify code - Time spent today: 1:30 (wasted time trying to get assertJ working)

---
+++ Java/src/main/com/gildedrose/GildedRose.java
@@ -0,0 +1,57 @@
+package com.gildedrose;
+
+class GildedRose {
+    static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    static final String CONJURED = "Conjured";
+    static final String AGED_BRIE = "Aged Brie";
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                if (item.quality > 0 && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                    item.quality = item.quality - 1;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+
+            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                        if (item.quality > 0 && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                            item.quality = item.quality - 1;
+                        }
+                    } else {
+                        item.quality = 0;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Java/src/test/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,79 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+    @Test
+    public void validateQualityRules() {
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Amazing Guacamole", 0, 10), //
+                new Item(GildedRose.AGED_BRIE, 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80), //
+                new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, -1, 80),
+                new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 15, 20),
+                new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 10, 49),
+                new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 5, 49),
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+        // Degrades normally for a standard item before sell-by date
+        assertEquals(19, items[0].quality, "Expecting the quality for item 0 to be reduced by 1");
+
+        // Degrades 2x faster for a standard item past sell-by date
+        assertEquals(8, items[1].quality, "Expecting the quality of an item past sell-by date to drop 2x faster");
+
+        // Increases in quality for aged brie and concert tickets BEFORE sell-by date -10
+        assertEquals(1, items[2].quality, "Aged brie should increase in quality");
+        assertEquals(21, items[6].quality, "Concert tickets increase quality");
+
+        // Quality never goes above 50
+        // Increases in quality x2 for concert tickets before sell-by date - 10
+        assertEquals(50, items[7].quality, "Concert tickets quality should increase 2x when sell-by date is less or equals to 10, but remain below 50");
+
+        // Increases in quality 3x for concert tickets after sell-by date - 10
+        assertEquals(50, items[8].quality, "Concert tickets quality should increase 1x when sell-by date is less or equals to 5, but remain below 50");
+
+        // Conjured items decrease in quality 2x faster
+        assertEquals(4, items[9].quality, "Conjured items decrease in quality 2x faster");
+
+        // Sulfuras quality doesn't change. Its sell-by date doesn't change either
+        assertEquals(80, items[4].quality);
+        assertEquals(0, items[4].sellIn);
+        assertEquals(80, items[5].quality);
+        assertEquals(-1, items[5].sellIn);
+
+        // Quality becomes 0 for concert tickets after sell by date, check quality increase too
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        assertEquals(25, items[6].quality, "Concert tickets increase quality");
+        app.updateQuality();
+        assertEquals(0, items[8].quality, "Concert tickets drop quality to 0 after sell-by date");
+        assertEquals(27, items[6].quality, "Concert tickets quality should increase 2x when sell-by date is less or equals to 10");
+
+        // Quality never goes below 0
+        assertEquals(0, items[9].quality, "Conjured items decrease in quality 2x faster but never below 0");
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        assertEquals(0, items[1].quality, "Quality never goes below 0");
+
+    }
+}
