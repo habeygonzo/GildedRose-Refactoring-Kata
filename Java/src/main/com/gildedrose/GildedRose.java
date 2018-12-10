aaad8c4d0654b6e3e85924a902124702931339d4
habeygonzo <habeygonzo@ice-wind.be>
2018-12-10 21:07:02 +0100

    - Refactored to avoid too many nested logic switches - I still think this isn't easy to maintain, but would need to modify the Item class for that and the goblin won't allow me to - Time spent: 15 minutes
    Some thoughts: modifying the Item class, or turning it into an interface, would allow to have several classes, each one handling its own SellIn and Quality values (and anything else). This would prevent unnecesary blocks of nested logic switches, and allow the usage of streaming and lambdas, which in this case would make reading and maintaining the code easier.

--- Java/src/main/com/gildedrose/GildedRose.java
+++ Java/src/main/com/gildedrose/GildedRose.java
@@ -1,8 +1,5 @@
 package com.gildedrose;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class GildedRose {
     static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
@@ -12,67 +9,74 @@ class GildedRose {
     static final int MAX_QUALITY = 50;
     static final int MIN_QUALITY = 0;
 
-    List<Item> qualityIncreases;
-    List<Item> qualityIncreasesVariableThenDrops;
-    List<Item> qualityDoesntChange;
-
     Item[] items;
 
     public GildedRose(Item[] items) {
-        this.qualityIncreases = new ArrayList<>();
-        this.qualityIncreasesVariableThenDrops = new ArrayList<>();
-        this.qualityDoesntChange = new ArrayList<>();
-
-        // Using Lists in case new items are added with similar abilities, or if we load the list from a file
-        this.qualityIncreases.add(new Item(GildedRose.AGED_BRIE, 0, 0));
-        this.qualityDoesntChange.add(new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80));
-        this.qualityIncreasesVariableThenDrops.add(new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 15, 20));
-
         this.items = items;
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (item.quality < MAX_QUALITY) {
-                    item.quality = item.quality + 1;
+            processQuality(item);
+            processSellIn(item);
+            processSellInExpired(item);
+        }
+    }
 
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        if (item.sellIn < 11 && item.quality < MAX_QUALITY) {
-                            item.quality++;
-                        }
-                        if (item.sellIn < 6 && item.quality < MAX_QUALITY) {
-                            item.quality++;
-                        }
-                    }
+    private void processSellInExpired(Item item) {
+        if (item.sellIn < 0) {
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.quality < MAX_QUALITY) {
+                    item.quality++;
                 }
             } else {
                 if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                     item.quality--;
-                    if (item.name.startsWith(CONJURED)) {
-                        item.quality--;
-                    }
+                    processSellInExpiredForConcertTickets(item);
                 }
             }
+        }
+    }
 
-            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                item.sellIn = item.sellIn - 1;
+    private void processSellInExpiredForConcertTickets(Item item) {
+        if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT) && item.quality > MIN_QUALITY) { // I'm not sure which is more expensive: comparing to 0 or assigning 0
+            item.quality = MIN_QUALITY;
+        }
+    }
+
+    private void processSellIn(Item item) {
+        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void processQuality(Item item) {
+        if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            if (item.quality < MAX_QUALITY) {
+                item.quality = item.quality + 1;
+                processQualityForConcertTickets(item);
             }
+        } else {
+            if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                item.quality--;
+                processQualityForConjuredItems(item);
+            }
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < MAX_QUALITY) {
-                        item.quality++;
-                    }
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        item.quality = MIN_QUALITY;
-                    } else {
-                        if (item.quality > MIN_QUALITY && !item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            item.quality--;
-                        }
-                    }
-                }
+    private void processQualityForConjuredItems(Item item) {
+        if (item.name.startsWith(CONJURED)) { // Decrease twice
+            item.quality--;
+        }
+    }
+
+    private void processQualityForConcertTickets(Item item) {
+        if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            if (item.sellIn < 11 && item.quality < MAX_QUALITY) { // Increase again (2x)
+                item.quality++;
+            }
+            if (item.sellIn < 6 && item.quality < MAX_QUALITY) { // Increase again (3x)
+                item.quality++;
             }
         }
     }
